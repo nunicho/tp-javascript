60a6c2c27eaa4dd94c14fd588f35e2cc90c66a8e
Mauricio <conta.alonso@gmail.com>
2022-06-29 21:40:04 -0300

    Terminado ejercicio 4.

---
+++ js/ejercicio4.js
@@ -0,0 +1,3 @@
+// 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+let nombreUsuario =prompt('¿Cómo te llamas?');
+document.write('Hola '+ nombreUsuario);
