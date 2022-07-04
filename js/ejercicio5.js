bc67b232fec77f55e0dd7b8805ad37598eddf869
Mauricio <conta.alonso@gmail.com>
2022-07-03 21:19:44 -0300

    Terminado ejercicio 6.

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,7 +1,10 @@
 /* 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
  Nota: Tener en cuenta la siguiente función: parseInt
  https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt */
+
+
 let valorUno = parseInt(prompt('Ingresar primer valor'),10);
 let valorDos = parseInt(prompt('Ingresar segundo valor'),10);
 document.write('Resultado: ' + (valorUno+valorDos))
+
 
---
+++ js/ejercicio6.js
@@ -0,0 +1,12 @@
+let valor1 = prompt("Ingresar primer valor");
+let valor2 = prompt("Ingresar segundo valor");
+
+if (valor1 > valor2){
+    document.write("El número mayor es: "+ valor1);
+}
+else if(valor1 == valor2){
+    document.write("los dos números son iguales: " + valor1 + " - " + valor2);
+}
+else{
+    document.write ("El número mayor es: "+ valor2)
+}
