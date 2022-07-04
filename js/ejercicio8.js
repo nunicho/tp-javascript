1930e0de7b791d1956aafe5195aa9dd45efc6187
Mauricio <conta.alonso@gmail.com>
2022-07-03 23:05:45 -0300

    Terminado ejercicio 8.

---
+++ js/ejercicio8.js
@@ -0,0 +1,8 @@
+let valorADividir = prompt("Ingresar valor a dividir: ");
+
+if (valorADividir%2==0){
+    document.write("El número " + valorADividir + " es divisible por 2")
+}
+else{
+    document.write("El número " + valorADividir +  " no es divisible por 2")
+}
