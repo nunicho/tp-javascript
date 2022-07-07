0a9572097277f3197374651d77bee37ee571f295
Mauricio <conta.alonso@gmail.com>
2022-07-06 21:35:58 -0300

    Ajustes de formato

--- js/ejercicio9B.js
+++ js/ejercicio9B.js
@@ -1,47 +1,50 @@
 // 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
 // Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
-// Realizado SIGUIENDO LOS LINEAMIENTOS VISTOS EN clase del 5/7/22
+// ESTA FUE MI INVESTIGACIÓN Y SOLUCIÓN antes de que lo revisáramos en la clase del 5/7/22
 
-let texto = prompt("Ingrese una frase de HASTA 10 caracteres:");
-document.write(texto);
-frase = texto.toLowerCase();
-let fraseLength = ("<br> Cantidad de letras" + frase.length);
-document.write("<br> La frase tiene la siguiente cantidad de letras: " + frase.length)
+let texto = prompt("Ingrese texto: ")
 
-if (frase.length < 11) {
-    document.write("<br> Las vocales son las siguientes: ")
-    if (frase.charAt(0) === 'a' || frase.charAt(0) === 'e' || frase.charAt(0) === 'i' || frase.charAt(0) === 'o' || frase.charAt(0) === 'u' || frase.charAt(0) === 'á' || frase.charAt(0) === 'é' || frase.charAt(0) === 'í' || frase.charAt(0) === 'ó' || frase.charAt(0) === 'ú') {
-        document.write(frase.charAt(0))
-    }
-    if (frase.charAt(1) === 'a' || frase.charAt(1) === 'e' || frase.charAt(1) === 'i' || frase.charAt(1) === 'o' || frase.charAt(1) === 'u' || frase.charAt(1) === 'á' || frase.charAt(1) === 'é' || frase.charAt(1) === 'í' || frase.charAt(1) === 'ó' || frase.charAt(1) === 'ú') {
-        document.write(frase.charAt(1))
-    }
-    if (frase.charAt(2) === 'a' || frase.charAt(2) === 'e' || frase.charAt(2) === 'i' || frase.charAt(2) === 'o' || frase.charAt(2) === 'u' || frase.charAt(2) === 'á' || frase.charAt(2) === 'é' || frase.charAt(2) === 'í' || frase.charAt(2) === 'ó' || frase.charAt(2) === 'ú') {
-        document.write(frase.charAt(2))
-    }
-    if (frase.charAt(3) === 'a' || frase.charAt(3) === 'e' || frase.charAt(3) === 'i' || frase.charAt(3) === 'o' || frase.charAt(3) === 'u' || frase.charAt(3) === 'á' || frase.charAt(3) === 'é' || frase.charAt(3) === 'í' || frase.charAt(3) === 'ó' || frase.charAt(3) === 'ú') {
-        document.write(frase.charAt(3))
-    }
-    if (frase.charAt(4) === 'a' || frase.charAt(4) === 'e' || frase.charAt(4) === 'i' || frase.charAt(4) === 'o' || frase.charAt(4) === 'u' || frase.charAt(4) === 'á' || frase.charAt(4) === 'é' || frase.charAt(4) === 'í' || frase.charAt(4) === 'ó' || frase.charAt(4) === 'ú') {
-        document.write(frase.charAt(4))
-    }
-    if (frase.charAt(5) === 'a' || frase.charAt(5) === 'e' || frase.charAt(5) === 'i' || frase.charAt(5) === 'o' || frase.charAt(5) === 'u' || frase.charAt(5) === 'á' || frase.charAt(5) === 'é' || frase.charAt(5) === 'í' || frase.charAt(5) === 'ó' || frase.charAt(5) === 'ú') {
-        document.write(frase.charAt(5))
-    }
-    if (frase.charAt(6) === 'a' || frase.charAt(6) === 'e' || frase.charAt(6) === 'i' || frase.charAt(6) === 'o' || frase.charAt(6) === 'u' || frase.charAt(6) === 'á' || frase.charAt(6) === 'é' || frase.charAt(6) === 'í' || frase.charAt(6) === 'ó' || frase.charAt(6) === 'ú') {
-        document.write(frase.charAt(6))
-    }
-    if (frase.charAt(7) === 'a' || frase.charAt(7) === 'e' || frase.charAt(7) === 'i' || frase.charAt(7) === 'o' || frase.charAt(7) === 'u' || frase.charAt(7) === 'á' || frase.charAt(7) === 'é' || frase.charAt(7) === 'í' || frase.charAt(7) === 'ó' || frase.charAt(7) === 'ú') {
-        document.write(frase.charAt(7))
-    }
-    if (frase.charAt(8) === 'a' || frase.charAt(8) === 'e' || frase.charAt(8) === 'i' || frase.charAt(8) === 'o' || frase.charAt(8) === 'u' || frase.charAt(8) === 'á' || frase.charAt(8) === 'é' || frase.charAt(8) === 'í' || frase.charAt(8) === 'ó' || frase.charAt(8) === 'ú') {
-        document.write(frase.charAt(8))
-    }
-    if (frase.charAt(9) === 'a' || frase.charAt(9) === 'e' || frase.charAt(9) === 'i' || frase.charAt(9) === 'o' || frase.charAt(9) === 'u' || frase.charAt(9) === 'á' || frase.charAt(9) === 'é' || frase.charAt(9) === 'í' || frase.charAt(9) === 'ó' || frase.charAt(9) === 'ú') {
-        document.write(frase.charAt(9))
-    }
-}
-else {
-    document.write("<br>La frase ingresada tiene más de 10 caracteres")
-}
+document.write("El texto ingresado fue :" + texto)
 
+let textoMinuscula = texto.toLowerCase();
+
+let textoNoConsonantes = textoMinuscula.replace(/[^aeiouáéíóú]/g, "")
+
+document.write(" <br>Las vocales de este texto son: "+textoNoConsonantes +"<br>")   
+
+
+let countTotal = textoNoConsonantes.split("").length;
+document.write("Cantidad de vocales TOTAL: " + countTotal +"<br>");
+
+let countA = (textoNoConsonantes.split("a").length -1)+(textoNoConsonantes.split("á").length -1);
+document.write("Cantidad de vocales A: " + countA +"<br>");
+
+let countE = (textoNoConsonantes.split("e").length  - 1)+(textoNoConsonantes.split("é").length  - 1);
+document.write("Cantidad de vocales E: " + countE +"<br>");
+
+let countI = (textoNoConsonantes.split("i").length  - 1)+(textoNoConsonantes.split("í").length  - 1);
+document.write("Cantidad de vocales I: " + countI +"<br>");
+
+let countO = (textoNoConsonantes.split("o").length  - 1)+(textoNoConsonantes.split("ó").length  - 1);
+document.write("Cantidad de vocales O: " + countO +"<br>");
+
+let countU = (textoNoConsonantes.split("u").length - 1)+(textoNoConsonantes.split("ú").length  - 1);
+document.write("Cantidad de vocales U: " + countU +"<br>");
+
+
+/*
+A la mecánica replace la aprendí de:
+https://bobbyhadz.com/blog/javascript-remove-special-characters-from-string
+
+La mecánica es str.replace[^/g] 
+Donde:
+a) str: es mi variable texto, ingresada por el usuario mediante el prompt y convertida a minúsculas (toLowerCase) para reducir variabilidad.
+b) replace: es la mecánica para reemplazar caracteres. 
+c) Los [] indican todos los caracteres a tener en cuenta en la función replace.
+d) El símbolo ^ indica que los siguientes caracteres no sean reemplazados. En mi caso, las vocales con y sin acentos. Por contraposición, todos los demás se reemplazan por " ".
+e) La legra g indica que reemplace todas las coincidencias dentro de la string. 
+
+Finalmente, usé length, una de las funciones indicadas por el enunciado, para contar cada una de las letras. 
+La función split la usé para separar el carácter a contar y length para contar dicho caracter. Investigando encontré que debía agregar -1 al final de la función, caso contrario no funciona. 
+
+*/
